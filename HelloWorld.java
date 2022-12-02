ba82d817e68d36f51c714deb4e036e924427b1d5
MD SHAHIM YAWAR ALI ANSARI <114904722+shahimYawar@users.noreply.github.com>
2022-12-02 18:23:35 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,1 +1,10 @@
-chghsgjgsdyfhis
+//Hello world Program in Java
+
+public class HelloWorld {
+	public static void main(String args[]) {
+			System.out.println("Hello World!");
+	}
+}
+
+//The Output will be:
+Hello World!
